20938b22a169cf4c6643d23dd31c07db1035d1b2
bjosch <github@bjosch.tk>
2023-03-23 18:44:02 +0100

    Adding more finegrained exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -16,3 +16,35 @@ class AddressComponentNotFound(AddressComponentException):
 
 class ComponentThresholdNotReached(AddressComponentException):
     pass
+
+
+class RuleException(BuacheException):
+    pass
+
+
+class ConditionalException(RuleException):
+    pass
+
+
+class MissingCriteriaError(RuleException):
+    pass
+
+
+class MissingConstantError(RuleException):
+    pass
+
+
+class InvalidOperatorError(ConditionalException):
+    pass
+
+
+class MissingRuleDeclarationForComponent(RuleException):
+    pass
+
+
+class ToManyRulesDeclaredForComponent(RuleException):
+    pass
+
+
+class AmbiguousScoresException(AddressException):
+    pass
