c261cff6bffc0191f615b1e0b3a4b7caaaad6254
Björn Schrammel <1463198+i3iorn@users.noreply.github.com>
2023-03-29 19:14:23 +0200

    Removed v1 since it does not offer anyng further

--- v1/augmentor.py
+++
@@ -1,7 +0,0 @@
-class Augmentor:
-    def __init__(self, name: str):
-        self.name = name
-
-    def augment(self, data: dict) -> dict:
-        # TODO: Implement data augmentation logic
-        return data
